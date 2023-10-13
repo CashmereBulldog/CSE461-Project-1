985f1ca1cf8ab3e030eaf05308ba5667556de62b
Michael <lynnmichael777@yahoo.com>
2023-10-13 13:37:07 -0700

    Configure test for main using pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
 import pytest
+import sys
+sys.path.append("./src")
+import main
 
 def test_main():
-  main()
+  main.main()
 
   # Use pytest.raises without specifying an exception type to assert no exception
   with pytest.raises(Exception, match=""):

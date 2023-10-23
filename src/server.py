f9312b15aea12ff8b6597ed0cb1ee5befb5a75c8
Reed Hamilton <rhamilt@uw.edu>
2023-10-22 18:37:09 -0700

    Stage B mostly working

--- src/server.py
+++ src/server.py
@@ -76,7 +76,7 @@ def stage_a():
     else:
         print ("Client message was not formatted correctly")
 
-    return (num, length, udp_port, secret_a, student_id)
+    stage_b(num, length, udp_port, secret_a, student_id)
 
 
 def stage_c(tcp_port : int, secret_b : int, student_id : int):
@@ -121,6 +121,79 @@ def stage_c(tcp_port : int, secret_b : int, student_id : int):
             listener.close()
 
 
+def stage_b(num, length, udp_port, secret_a, student_id):
+    print("num is ", num)
+    print("udp port is", udp_port)
+    listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind to address and IP
+    listener.bind(("localhost", udp_port))
+    print("Listening on port:", udp_port)
+
+    iteration = 0
+    secret_b = None
+    tcp_port = None
+    print("Before start of while loop, SERVER B")
+    while True:
+        padding = 4 - (length % 4)
+        data, client_addr = listener.recvfrom(12 + (length + 4) + padding)
+        # Length of payload stored in first 4 bytes (header is 12 bytes)
+        payload_int = int.from_bytes(data[:4], byteorder='big')
+        # if len(data) - 12 != payload_int + 4 + padding:
+        #     print("Length of payload not equal to len + 4")
+        #     break
+        if len(data) % 4 != 0:
+            print("len is ", len(data))
+            print("Length of packet is not divisible by 4")
+            break
+
+        headerCheck = check_header(data[:12], length + 4, secret_a)
+        if headerCheck == False:
+            print('badly formatted header')
+            break
+
+        # Payload: First 4 bytes contains  integer identifying the packet.
+        # The first packet should have this identifier set to 0,
+        # while the last packet should have its counter set to num-1
+        first_4_bytes = int.from_bytes(data[12:16], byteorder="big")
+        print (first_4_bytes)
+        if first_4_bytes != iteration:
+            print("Iteration is incorrect")
+            continue
+        # Check that the rest of the packet is filled with zeros:
+        last_bytes = data[16:]
+        # if not b'\0' * (length + padding) != data[16:]:
+        #     print (b'\0' * (length + padding))
+        #     print (data[16:])
+        #     print("The rest of the payload should be filled with 0s")
+        #     break
+
+        # Randomly decide if ack should be sent.
+        to_send = randint(0, 2)
+        if to_send > 0:
+            print("Decided to send ack")
+            # Include the payload identifier of packet in ack.
+            message = generate_header(4, secret_a, 2, student_id) + data[12:16]
+            listener.sendto(message, client_addr)
+            # Only increase iteration if ack was sent.
+            iteration = iteration + 1
+        else:
+            print("Decided not to send ack")
+
+        if iteration == num:
+            # All num packets were received. Send tcp port number, and a secretB.
+            secret_b = randint(0, 500)
+            tcp_port = randint(1024, 65353)
+            break
+
+    print("Closing socket")
+    print("***** STAGE B *****\n")
+    listener.close()
+    print("Secret_b", secret_b)
+    print("tcp_port", tcp_port)
+    return secret_b, tcp_port
+
+
 def stage_d(num2, len2, secret_c, c, student_id, connection):
     packets_received = 0
     valid = True
@@ -146,85 +219,10 @@ def stage_d(num2, len2, secret_c, c, student_id, connection):
     print ("Done!")
 
 
-    return (num, length, udp_port, secret_a, student_id)
-
-def stage_b(num, udp_port, secret_a):
-    print("num is ", num)
-    print("udp port is", udp_port)
-    listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Bind to address and IP
-    listener.bind(("localhost", udp_port))
-    print("Listening on port:", udp_port)
-
-    buffer = b''
-    iteration = 0
-    secret_b = None
-    tcp_port = None
-    print("Before start of while loop, SERVER B")
-    while True:
-        data, client_addr = listener.recvfrom(1024)
-        print(data)
-        buffer += data
-        # Length of payload stored in first 4 bytes (header is 12 bytes)
-        payload_int = int.from_bytes(buffer[:4], byteorder='little')
-        if len(data) - 12 != payload_int + 4:
-            print("Length of payload not equal to len + 4")
-            break
-        if len(data) % 4 != 0:
-            print("len is ", len(data))
-            print("Length of packet is not divisible by 4")
-            break
-        expected_length = payload_int
-
-        headerCheck = check_header(data[:12],expected_length, secret_a)
-        if headerCheck == False:
-            print('badly formatted header')
-            break
-
-        # Payload: First 4 bytes contains  integer identifying the packet.
-        # The first packet should have this identifier set to 0,
-        # while the last packet should have its counter set to num-1
-        first_4_bytes = buffer[11:16]
-        if first_4_bytes != iteration:
-            print("Iteration is incorrect")
-            break
-        # Check that the rest of the packet is filled with zeros:
-        last_bytes = buffer[16:]
-        if not all(byte == 0 for byte in last_bytes):
-            print("The rest of the payload should be filled with 0s")
-
-        # Randomly decide if ack should be sent.
-        to_send = randint(0, 2)
-        if to_send > 0:
-            print("Decided to send ack")
-            # Include the payload identifier of packet in ack.
-            message = bytes('ack').encode('utf-8')
-            listener.sendto(message, client_addr)
-            # Only increase iteration if ack was sent.
-            iteration = iteration + 1
-        else:
-            print("Decided not to send ack")
-
-        if iteration == num:
-            # All num packets were received. Send tcp port number, and a secretB.
-            secret_b = randint(0, 500)
-            tcp_port = randint(1024, 65353)
-            break
-
-    print("Closing socket")
-    print("***** STAGE B *****\n")
-    listener.close()
-    print("Secret_b", secret_b)
-    print("tcp_port", tcp_port)
-    return secret_b, tcp_port
-
-
 
 def main():
     """ Main function that calls the stages for the server """
-    num, length, udp_port, secret_a, student_id = stage_a()
-    secret_b, tcp_port = stage_b(num, udp_port, secret_a)
+    stage_a()
 
 
 if __name__ == "__main__":

586af2153f37cd3c569edc09105c5b8cd37b1946
Reed Hamilton <rhamilt@uw.edu>
2023-11-18 10:22:56 -0800

    Add part 2 controller

--- src/project2/part2/part2controller.py
+++ src/project2/part2/part2controller.py
@@ -5,6 +5,7 @@
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
+import pox.lib.packet as pkt
 
 log = core.getLogger()
 
@@ -23,7 +24,32 @@ class Firewall(object):
         # This binds our PacketIn event listener
         connection.addListeners(self)
 
-        # add switch rules here
+        # disallow ip_v4 traffic
+        # ipv4_flow = of.ofp_flow_mod()
+        # ipv4_flow.match = of.ofp_match(dl_type=0x8000)
+    
+        action_flood = of.ofp_action_output(port=of.OFPP_FLOOD)
+
+        # allow icmp
+        icmp_new_fm = of.ofp_flow_mod()
+        icmp_new_fm.match = of.ofp_match(dl_type=0x0800)
+        icmp_new_fm.actions.append(action_flood)
+        # only allow icmp
+        icmp_new_fm.match.nw_proto = 1
+
+        # allow arp
+        arp_new_fm = of.ofp_flow_mod()
+        arp_new_fm.match = of.ofp_match(dl_type=0x0806)
+        arp_new_fm.actions.append(action_flood)
+
+        # disallow ipv4
+        ipv4_flow = of.ofp_flow_mod()
+        ipv4_flow.actions.append(of.ofp_action_output(port=of.OFPP_NONE))
+
+        self.connection.send(icmp_new_fm)
+        self.connection.send(arp_new_fm)
+        self.connection.send(ipv4_flow)
+
 
     def _handle_PacketIn(self, event):
         """

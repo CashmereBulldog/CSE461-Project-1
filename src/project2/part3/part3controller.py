1723b1c8df4589d10b910f5f93abeacc0af6f386
Reed Hamilton <rhamilt@uw.edu>
2023-11-21 16:58:55 -0800

    Finish part 3 controller

--- src/project2/part3/part3controller.py
+++ src/project2/part3/part3controller.py
@@ -39,10 +39,10 @@ class Part3Controller(object):
         # send it messages!
         self.connection = connection
 
-        arp_new_fm = of.ofp_flow_mod()
-        arp_new_fm.match = of.ofp_match(dl_type=0x0806)
-        arp_new_fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-        self.connection.send(arp_new_fm)
+        arp_fm = of.ofp_flow_mod()
+        arp_fm.match = of.ofp_match(dl_type=0x0806)
+        arp_fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(arp_fm)
 
         # This binds our PacketIn event listener
         connection.addListeners(self)
@@ -62,105 +62,43 @@ class Part3Controller(object):
             exit(1)
 
     def s1_setup(self):
-        h10_fm = of.ofp_flow_mod()
-        h10_fm.match = of.ofp_match(dl_type=0x0800)
-        h10_fm.match.nw_dst = SUBNETS["h10"]
-        h10_fm.actions.append(of.ofp_action_output(port=10))
-        self.connection.send(h10_fm)
-
-        from_h10_fm = of.ofp_flow_mod()
-        from_h10_fm.match = of.ofp_match(dl_type=0x0800)
-        from_h10_fm.match.nw_src = SUBNETS["h10"]
-        from_h10_fm.actions.append(of.ofp_action_output(port=20))
-        self.connection.send(from_h10_fm)
+        fm = of.ofp_flow_mod()
+        fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(fm)
 
     def s2_setup(self):
-        # put switch 2 rules here
-        h20_fm = of.ofp_flow_mod()
-        h20_fm.match = of.ofp_match(dl_type=0x0800)
-        h20_fm.match.nw_src = SUBNETS["h20"]
-        h20_fm.actions.append(of.ofp_action_output(port=20))
-        self.connection.send(h20_fm)
-
-        from_h20_fm = of.ofp_flow_mod()
-        from_h20_fm.match = of.ofp_match(dl_type=0x0800)
-        from_h20_fm.match.nw_src = SUBNETS["h20"]
-        from_h20_fm.actions.append(of.ofp_action_output(port=10))
-        self.connection.send(from_h20_fm)
+        fm = of.ofp_flow_mod()
+        fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(fm)
 
     def s3_setup(self):
-        '''
-        # put switch 3 rules here
-        h30_fm = of.ofp_flow_mod()
-        h30_fm.match.nw_dst = SUBNETS["h30"]
-        h30_fm.actions.append(of.ofp_action_output(port=30))
-        self.connection.send(h30_fm)
-
-        from_h30_fm = of.ofp_flow_mod()
-        from_h30_fm.match.nw_src = SUBNETS["h30"]
-        from_h30_fm.actions.append(of.ofp_action_output(port=10))
-        self.connection.send(from_h30_fm)
-        '''
+        fm = of.ofp_flow_mod()
+        fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(fm)
 
     def cores21_setup(self):
-        # Don't allow icmp traffic from hnotrust
-        '''
-        icmp_notrust_fm = of.ofp_flow_mod()
-        icmp_notrust_fm.match = of.ofp_match(dl_type=0x0800)
-        icmp_notrust_fm.match.nw_proto = 1
-        icmp_notrust_fm.match.nw_src = SUBNETS["hnotrust"]
-        icmp_notrust_fm.actions.append(of.ofp_action_output(port=of.OFPP_NONE))
-        self.connection.send(icmp_notrust_fm)
+        notrust_fm = of.ofp_flow_mod()
+        notrust_fm.match = of.ofp_match(dl_type=0x0800)
+        notrust_fm.match.nw_proto = 1
+        notrust_fm.match.nw_src = SUBNETS["hnotrust"]
+        notrust_fm.actions.append(of.ofp_action_output(port=0))
+        self.connection.send(notrust_fm)
 
-        ip_hnotrust_fm = of.ofp_flow_mod()
-        ip_hnotrust_fm.match.nw_dst = SUBNETS["hnotrust"]
-        ip_hnotrust_fm.actions.append(of.ofp_action_output(port=1))
-        self.connection.send(ip_hnotrust_fm)
-        '''
-
-        ip_h10_fm = of.ofp_flow_mod()
-        ip_h10_fm.match = of.ofp_match(dl_type=0x0800)
-        ip_h10_fm.match.nw_dst = SUBNETS["h10"]
-        ip_h10_fm.actions.append(of.ofp_action_output(port=10))
-        self.connection.send(ip_h10_fm)
-
-        ip_h20_fm = of.ofp_flow_mod()
-        ip_h20_fm.match = of.ofp_match(dl_type=0x0800)
-        ip_h20_fm.match.nw_dst = SUBNETS["h20"]
-        ip_h20_fm.actions.append(of.ofp_action_output(port=20))
-        self.connection.send(ip_h20_fm)
-
-        '''
-        ip_h30_fm = of.ofp_flow_mod()
-        ip_h30_fm.match.nw_dst = SUBNETS["h30"]
-        ip_h30_fm.actions.append(of.ofp_action_output(port=30))
-        self.connection.send(ip_h30_fm)
-
-        ip_dc_fm = of.ofp_flow_mod()
-        ip_dc_fm.match.nw_dst = SUBNETS["serv1"]
-        ip_dc_fm.actions.append(of.ofp_action_output(port=31))
-        self.connection.send(ip_dc_fm)
-        '''
+        fm = of.ofp_flow_mod()
+        fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(fm)
 
     def dcs31_setup(self):
-        '''
-        # put datacenter switch rules here
-        icmp_notrust_fm = of.ofp_flow_mod()
-        icmp_notrust_fm.match = of.ofp_match(dl_type=0x0800)
-        icmp_notrust_fm.match.nw_src = SUBNETS["hnotrust"]
-        icmp_notrust_fm.actions.append(of.ofp_action_output(port=of.OFPP_NONE))
-        self.connection.send(icmp_notrust_fm)
+        notrust_fm = of.ofp_flow_mod()
+        notrust_fm.match = of.ofp_match(dl_type=0x0800)
+        notrust_fm.match.nw_src = SUBNETS["hnotrust"]
+        notrust_fm.actions.append(of.ofp_action_output(port=0))
+        self.connection.send(notrust_fm)
 
-        h31_fm = of.ofp_flow_mod()
-        h31_fm.match.nw_dst = SUBNETS["serv1"]
-        h31_fm.actions.append(of.ofp_action_output(port=31))
-        self.connection.send(h31_fm)
-
-        from_h31_fm = of.ofp_flow_mod()
-        from_h31_fm.match.nw_src = SUBNETS["serv1"]
-        from_h31_fm.actions.append(of.ofp_action_output(port=10))
-        self.connection.send(from_h31_fm)
-        '''
+        fm = of.ofp_flow_mod()
+        fm.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(fm)
+       
 
     # used in part 4 to handle individual ARP packets
     # not needed for part 3 (USE RULES!)
